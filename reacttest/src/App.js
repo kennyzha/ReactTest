4a3e3b81e429979ace942f87a6b8f2239fed1abe
Kenny <kzhao7301@gmail.com>
2018-03-04 12:43:16 -0500

    created list of projects

---
+++ reacttest/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects'
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+       {
+          title: 'Business Website',
+          category: 'Web Design'
+        },{
+          title: 'Social App',
+          category: 'Mobile Design'
+        },{
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+    ]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        My App
+        <Projects projects={this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;
